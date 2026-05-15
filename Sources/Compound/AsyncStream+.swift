370af8760c63728cc8c80bc21b6c6bd874305276
indextrown <indextrown@gmail.com>
2026-05-16 00:14:58 +0900

    stream: AsyncStream mutation helper 추가

--- Sources/Compound/AsyncStream+.swift
+++ Sources/Compound/AsyncStream+.swift
@@ -6,3 +6,58 @@
 //
 
 import Foundation
+
+public extension AsyncStream where Element: Sendable {
+    static func just(_ element: Element) -> AsyncStream<Element> {
+        AsyncStream { continuation in
+            continuation.yield(element)
+            continuation.finish()
+        }
+    }
+}
+
+public extension AsyncStream where Element: Sendable {
+    static func concat(
+        _ streams: AsyncStream<Element>...
+    ) -> AsyncStream<Element> {
+        AsyncStream { continuation in
+            let task = Task {
+                for stream in streams {
+                    for await element in stream {
+                        continuation.yield(element)
+                    }
+                }
+                continuation.finish()
+            }
+
+            continuation.onTermination = { _ in
+                task.cancel()
+            }
+        }
+    }
+}
+
+public extension AsyncStream where Element: Sendable {
+    static func merge(
+        _ streams: AsyncStream<Element>...
+    ) -> AsyncStream<Element> {
+        AsyncStream { continuation in
+            let task = Task {
+                await withTaskGroup(of: Void.self) { group in
+                    for stream in streams {
+                        group.addTask {
+                            for await element in stream {
+                                continuation.yield(element)
+                            }
+                        }
+                    }
+                }
+                continuation.finish()
+            }
+
+            continuation.onTermination = { _ in
+                task.cancel()
+            }
+        }
+    }
+}
